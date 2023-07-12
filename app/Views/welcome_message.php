9751d44c5e41354d7d1b197135e6c44bff892875
Flávio E Pimentel <flavio.ericksan@hotmail.com>
2023-07-12 20:37:58 -0300

    style: :lipstick: Add Bootstrap link

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -5,11 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial</title>
-    <style>
-        <?php
-        require(APPPATH . "Views/src/style.css");
-        ?>
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<style>
+    <?php
+    require(APPPATH . "Views/src/style.css");
+    ?>
+</style>
 </head>
 
 <body>
@@ -17,24 +19,22 @@
         <nav>
             <ul class="menu">
                 <li><?php echo anchor('/', 'Home') ?></li>
-                <li><?php echo anchor('/user', 'Usuários') ?></li>
+                <li><?php echo anchor('/user', 'Convidados') ?></li>
             </ul>
         </nav>
     </div>
     <section>
         <div class="welcome__text">
-            <h2>Bem Vindo!</h2>
+            <h2>Bem Vindo! 🥾🎒🧗‍♂️🚵‍♀️🏕️🧭🗺️</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quas natus
                 perferendis repellat ea magni doloremque sapiente debitis iure
                 voluptatum explicabo? Voluptatem cumque tempore aperiam velit re
             </p>
+            <p class="call__to__action">Pronto para sua próxima aventura com seu amigos?</p>
         </div>
         <div class="welcome__image">
         </div>
     </section>
-    <div id="teste">
-        <p>zzz</p>
-    </div>
 
 </body>
 
